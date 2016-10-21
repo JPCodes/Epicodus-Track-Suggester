e6199a39b45613232ff411ab08ea4d42b06e9510
Jim Padilla <jp_code2013@yahoo.com>
2016-10-21 09:26:52 -0700

    Create jQuery to handle user inputs and output result based off of a score

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,35 @@
+
+var trackGen = function(q1, q2, q3, q4, q5, q6) {
+
+  var result = q1 + q2 + q3 + q4 + q5 + q6;
+
+  // C# 6-9 , Java 12-15 , PHP 18-21, Ruby 22-24
+
+  if (result >= 6 && result <= 9) {
+    return ".c-track";
+  } else if (result >= 12 && result <= 15) {
+    return ".java-track";
+  } else if (result >= 18 && result <= 21) {
+    return ".php-track";
+  } else if (result >= 22 && result <= 24) {
+    return ".ruby-track";
+  }
+
+};
+
 $(document).ready(function() {
 
   $("form#quiz").submit(function(event) {
-    var q1Input = parseInt($("#q1").val());
-    var q2Input = parseInt($("#q2").val());
-    var q3Input = parseInt($("#q3").val());
-    var q4Input = parseInt($("#q4").val());
-    var q5Input = parseInt($("#q5").val());
-    var q6Input = parseInt($("#q6").val());
+    var q1 = parseInt($("#q1").val());
+    var q2 = parseInt($("#q2").val());
+    var q3 = parseInt($("#q3").val());
+    var q4 = parseInt($("#q4").val());
+    var q5 = parseInt($("#q5").val());
+    var q6 = parseInt($("#q6").val());
 
-    
+    var answer = trackGen(q1, q2, q3, q4, q5, q6).toString();
+
+    $(answer).show();
 
     event.preventDefault();
 
