fd58adc329f34f0e6bce679444c29003047ab7e4
Jim Padilla <jp_code2013@yahoo.com>
2016-10-21 09:00:12 -0700

    Create all html, style color theme

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+  $("form#quiz").submit(function(event) {
+    var q1Input = parseInt($("#q1").val());
+    var q2Input = parseInt($("#q2").val());
+    var q3Input = parseInt($("#q3").val());
+    var q4Input = parseInt($("#q4").val());
+    var q5Input = parseInt($("#q5").val());
+    var q6Input = parseInt($("#q6").val());
+
+    
+
+    event.preventDefault();
+
+  });
+
+});
