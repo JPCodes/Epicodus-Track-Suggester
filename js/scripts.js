e36b6f121991375c35ed42ca0ec8255aa5ed8c1d
Jim Padilla <jp_code2013@yahoo.com>
2016-10-21 09:55:38 -0700

    Change scoring ranges slightly, update JavaScript to handle new scores, add hide function to jQuery section code

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,15 @@ var trackGen = function(q1, q2, q3, q4, q5, q6) {
 
   var result = q1 + q2 + q3 + q4 + q5 + q6;
 
-  // C# 6-9 , Java 12-15 , PHP 18-21, Ruby 22-24
+  // C# 6-9 , Java 10-14 , PHP 15-19, Ruby 20-24
 
-  if (result >= 6 && result <= 9) {
+  if (result > 5 && result < 10) {
     return ".c-track";
-  } else if (result >= 12 && result <= 15) {
+  } else if (result > 9 && result < 15) {
     return ".java-track";
-  } else if (result >= 18 && result <= 21) {
+  } else if (result > 14 && result < 20) {
     return ".php-track";
-  } else if (result >= 22 && result <= 24) {
+  } else if (result > 19) {
     return ".ruby-track";
   }
 
@@ -20,6 +20,7 @@ var trackGen = function(q1, q2, q3, q4, q5, q6) {
 $(document).ready(function() {
 
   $("form#quiz").submit(function(event) {
+    debugger;
     var q1 = parseInt($("#q1").val());
     var q2 = parseInt($("#q2").val());
     var q3 = parseInt($("#q3").val());
@@ -29,6 +30,7 @@ $(document).ready(function() {
 
     var answer = trackGen(q1, q2, q3, q4, q5, q6).toString();
 
+    $(".track").hide();
     $(answer).show();
 
     event.preventDefault();
